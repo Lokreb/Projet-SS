db1c5787c77f4df6bf4e4ed6dd1bd106bf0f9408
Lokreb <fouacheletelliermarvin@gmail.com>
2023-09-10 16:51:01 +0200

    {Update} Drag & Drop fonctionnel sur proto avec vérification des slots

--- Assets/_Scripts/DragDrop.cs
+++ Assets/_Scripts/DragDrop.cs
@@ -5,78 +5,83 @@ using UnityEngine.EventSystems;
 
 public class DragDrop : MonoBehaviour
 {
-   /* public GameObject objectDrag;
-    public GameObject ObjectDragToPos;
-
-    public float DropDistance;
-
-    public bool isLocked;
-
-    Vector2 objectInitPos;
-
-    void Start() {
-        objectInitPos = objectDrag.transform.position;
-    }
-
-    public void DragObject() {
-        if(!isLocked) {
-            objectDrag.transform.position = Input.mousePosition;
-        }
-    }
-
-    public void DropObject() {
-        float Distance = Vector3.Distance(objectDrag.transform.position, ObjectDragToPos.transform.position);
-        if(Distance < DropDistance) {
-            //isLocked = true;
-            objectDrag.transform.position = ObjectDragToPos.transform.position;
-        } else {
-            objectDrag.transform.position = objectInitPos;
-        }
-    }*/
-
     public GameObject objectDrag;
     public GameObject[] ObjectsDragToPos; // Utiliser un tableau d'objets
     public float DropDistance;
     public bool isLocked;
 
     Vector2 objectInitPos;
+    Transform originalParent; // Stocker le parent d'origine de l'objet
 
     void Start()
     {
         objectInitPos = objectDrag.transform.position;
+        originalParent = objectDrag.transform.parent; // Enregistrez le parent d'origine
     }
 
     public void DragObject()
     {
         if (!isLocked)
         {
-            objectDrag.transform.position = Input.mousePosition;
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePosition.z = 0; // Assurez-vous que la coordonnée Z est correcte
+
+            // Déplacez l'objet vers la position de la souris en coordonnées du monde
+            objectDrag.transform.position = mousePosition;
         }
     }
 
     public void DropObject()
     {
-        float minDistance = float.MaxValue;
-        GameObject nearestObject = null;
-
-        foreach (GameObject dropTarget in ObjectsDragToPos)
+        if (!isLocked)
         {
-            float distance = Vector3.Distance(objectDrag.transform.position, dropTarget.transform.position);
-            if (distance < minDistance)
+            float minDistance = float.MaxValue;
+            Transform nearestSlot = null;
+
+            foreach (GameObject dropTarget in ObjectsDragToPos)
             {
-                minDistance = distance;
-                nearestObject = dropTarget;
-            }
-        }
+                // Vérifiez si le slot est occupé
+                if (dropTarget.transform.childCount > 0)
+                {
+                    // Récupérez l'enfant du slot
+                    Transform child = dropTarget.transform.GetChild(0);
 
-        if (nearestObject != null && minDistance < DropDistance)
-        {
-            // isLocked = true;
-            objectDrag.transform.position = nearestObject.transform.position;
-        }
-        else
-        {
-            objectDrag.transform.position = objectInitPos;
+                    // Vérifiez si l'enfant est l'objet initial
+                    if (child == objectDrag.transform)
+                    {
+                        // Désengager l'enfant du slot
+                        child.SetParent(null);
+                    }
+                    else
+                    {
+                        continue; // Slot occupé par un autre objet, passez au suivant
+                    }
+                }
+
+                // Convertissez les coordonnées du monde 3D des emplacements de slot
+                Vector3 slotPosition = dropTarget.transform.position;
+
+                float distance = Vector3.Distance(objectDrag.transform.position, slotPosition);
+                if (distance < minDistance)
+                {
+                    minDistance = distance;
+                    nearestSlot = dropTarget.transform;
+                }
+            }
+
+            if (nearestSlot != null && minDistance < DropDistance)
+            {
+                //isLocked = true;
+                objectDrag.transform.position = nearestSlot.position;
+                // Mettez l'objet en tant qu'enfant du slot pour le verrouiller en place
+                objectDrag.transform.SetParent(nearestSlot);
+            }
+            else
+            {
+                objectDrag.transform.position = objectInitPos;
+                // Rétablissez le parent d'origine de l'objet (le canvas)
+                objectDrag.transform.SetParent(originalParent);
+            }
         }
     }
 }
